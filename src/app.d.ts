ed848acc3707e9489827e42233a9a26a14b2329b
Denis Donici <skryonline@gmail.com>
2025-12-01 09:04:39 +0200

    Initial commit

---
+++ src/app.d.ts
@@ -0,0 +1,22 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
+}
+
+declare module "remult" {
+  // interface UserInfo {
+  //   phone: string;
+  // }
+  // interface FieldOptions<entityType, valueType> {
+  //   placeholder?: string;
+  // }
+}
+
+export {};
---
+++ src/server/api.ts
@@ -0,0 +1,25 @@
+import { remultApi } from "remult/remult-sveltekit";
+import { SqlDatabase } from "remult";
+import { createClient } from "@libsql/client";
+import { TursoDataProvider } from "remult/remult-turso";
+
+import { auth } from "../demo/auth/server/index";
+import { env } from "$env/dynamic/private";
+
+export const api = remultApi({
+  dataProvider: new SqlDatabase(
+    new TursoDataProvider(
+      createClient({
+        url: env.TURSO_DATABASE_URL,
+        authToken: env.TURSO_AUTH_TOKEN,
+      }),
+    ),
+  ),
+  admin: true,
+  modules: [
+    auth({
+      // Add some roles to some users with env variable.
+      // SUPER_ADMIN_EMAILS
+    }),
+  ],
+});
